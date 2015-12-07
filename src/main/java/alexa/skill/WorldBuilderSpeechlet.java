9a1c93ef1e9a7dd2670fd0c7b0b93c7193cc278e
ranjiti <ranjiti@amazon.com>
2015-12-06 22:16:46 -0800

    First uber jar

---
+++ src/main/java/alexa/skill/WorldBuilderSpeechlet.java
@@ -0,0 +1,21 @@
+package alexa.skill;
+
+import com.amazon.speech.speechlet.*;
+
+public class WorldBuilderSpeechlet implements Speechlet {
+    public void onSessionStarted(SessionStartedRequest sessionStartedRequest, Session session) throws SpeechletException {
+
+    }
+
+    public SpeechletResponse onLaunch(LaunchRequest launchRequest, Session session) throws SpeechletException {
+        return null;
+    }
+
+    public SpeechletResponse onIntent(IntentRequest intentRequest, Session session) throws SpeechletException {
+        return null;
+    }
+
+    public void onSessionEnded(SessionEndedRequest sessionEndedRequest, Session session) throws SpeechletException {
+
+    }
+}
